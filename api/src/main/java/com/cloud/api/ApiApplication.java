cc81fd44adc229ef74d0899c9b801afb146076a1
lingjun.jlj <jlj_vip98@qq.com>
2018-09-14 19:35:07 +0800

    spring cloud

--- api/src/main/java/com/cloud/api/ApiApplication.java
+++ api/src/main/java/com/cloud/api/ApiApplication.java
@@ -2,11 +2,14 @@ package com.cloud.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author : lingjun.jlj
  * @description : 服务提供
  */
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiApplication {
 
